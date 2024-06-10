f25a60f8735812fb1305c48b2339a5a8fb27e3a0
lucas da silva cruz <lucas6jau@gmail.com>
2024-06-10 18:23:54 -0300

    refactor: use of Page Object in first test

--- src/test/java/site/MainTest.java
+++ src/test/java/site/MainTest.java
@@ -32,15 +32,14 @@ class MainTest {
     @Test
     @DisplayName("Should access power register page")
     void shouldAccessPowerRegisterPage() throws InterruptedException{
-        driver.get("https://site-tc1.vercel.app/");
+        HomePage homePage = new HomePage(driver);
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
         WebElement link = wait.until(ExpectedConditions.
-                elementToBeClickable(new By.ByXPath("/html/body/div/body/div/header/nav/ul/li/a")));
+                elementToBeClickable(homePage.getCadastrarButtom()));
         link.click();
-        final WebElement element = driver.
-                findElement(By.xpath("/html/body/div/body/div[2]/div/div/form/div[1]/label"));
-        assertThat(element.getText()).isEqualTo("Nome do Poder:");
+
         driver.close();
 
     }
