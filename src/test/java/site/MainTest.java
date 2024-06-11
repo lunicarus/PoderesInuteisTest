e7bfddd16c807f0501956c07f5e88a4a9f7d4dcd
BBranco97 <barbarabranco1997@gmail.com>
2024-06-11 14:55:24 -0300

    test: feat: implement Should create a new power

--- src/test/java/site/MainTest.java
+++ src/test/java/site/MainTest.java
@@ -3,17 +3,20 @@ package site;
 import com.github.javafaker.Faker;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 //import org.assertj.core.api.SoftAssertions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MainTest {
 
@@ -82,14 +85,39 @@ class MainTest {
             assertThat(currentUrl).contains("/cadastro");
         }
 
-        @Nested
-        class EditarPoderPageTest {
+        @Test
+        @DisplayName("Should create a new power")
+        void shouldCreateNewPower() {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1));
 
-        }
 
-        @Nested
-        class DeletarPoderPageTest {
+            WebElement link = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Cadastrar")));
+            link.click();
 
+            String nome = faker.superhero().power();
+            String descricao = faker.lorem().sentence();
+            String efeitosColaterais = faker.lorem().sentence();
+            int nota = faker.number().numberBetween(1, 6); //entre 1 e 5
+
+            WebElement nameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("nome_do_poder")));
+            WebElement descriptionInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("descricao")));
+            WebElement efeitosColateraisInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("efeitos_colaterais")));
+            WebElement notaSelect = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("nota")));
+
+            nameInput.sendKeys(nome);
+            descriptionInput.sendKeys(descricao);
+            efeitosColateraisInput.sendKeys(efeitosColaterais);
+            new Select(notaSelect).selectByValue(String.valueOf(nota)); // Seleciona a nota
+
+            WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit' and text()='Cadastrar Poder']")));
+
+            submitButton.click();
+
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            System.out.println("Alerta: " + alertText);
+            assertTrue(alertText.contains("Poder criado com sucesso!"));
+            alert.dismiss();
         }
     }
 }
