354af84611067b65ec64f5c08f1293241fdf4a3a
lucas da silva cruz <lucas6jau@gmail.com>
2024-06-12 01:10:19 -0300

    feat: methods to get location of components of home page

--- src/test/java/site/HomePage.java
+++ src/test/java/site/HomePage.java
@@ -1,12 +1,13 @@
 package site;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 public class HomePage {
     protected WebDriver driver;
 
     private final By header =  By.xpath("//*[@id=\"root\"]/body/div/header/h1/a");
     private final By cadastrarButtom = By.xpath("//*[@id=\"root\"]/body/div/header/nav/ul/li/a");
-
+    private final By powersList = By.id("powersList");
     public HomePage(WebDriver driver){
         this.driver = driver;
         driver.get("https://site-tc1.vercel.app/");
@@ -20,5 +21,17 @@ public class HomePage {
 
     public By getCadastrarButtom() {
         return cadastrarButtom;
+    }
+    public By getHeader() {
+        return header;
+    }
+    public Point getHeaderLocation(){
+        return driver.findElement(header).getLocation();
+    }
+    public Point getCadastrarButtomLocation(){
+        return driver.findElement(cadastrarButtom).getLocation();
+    }
+    public Point getPowersListLocation(){
+        return driver.findElement(powersList).getLocation();
     }
 }
