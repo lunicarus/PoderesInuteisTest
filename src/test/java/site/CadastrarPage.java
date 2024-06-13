3a3b7ea5ad76b80dc843cb4111d7cfd315f56b09
lucas da silva cruz <lucas6jau@gmail.com>
2024-06-12 21:50:57 -0300

    feat: get location methods for UI tests

--- src/test/java/site/CadastrarPage.java
+++ src/test/java/site/CadastrarPage.java
@@ -1,13 +1,13 @@
 package site;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.WebDriver;
 
 public class CadastrarPage {
     protected WebDriver driver;
     private final By header =  By.xpath("//*[@id=\"root\"]/body/div/header/h1/a");
     private final By cadastrarButtom = By.xpath("//*[@id=\"root\"]/body/div/header/nav/ul/li/a");
-    private final By powerForm = By.id("powerForm");
     private final By nomePoder = By.id("nome_do_poder");
     private final By descricao = By.id("descricao");
     private final By efeitosColaterais = By.id("efeitos_colaterais");
@@ -48,4 +48,25 @@ public class CadastrarPage {
         }
     }
 
+    public Rectangle getCadastrarButtomLocation() {
+        return driver.findElement(cadastrarButtom).getRect();
+    }
+    public Rectangle getHeaderLocation() {
+        return driver.findElement(header).getRect();
+    }
+    public Rectangle getEfeitosColateraisLocation() {
+        return driver.findElement(efeitosColaterais).getRect();
+    }
+    public Rectangle getNomePoderLocation() {
+        return driver.findElement(nomePoder).getRect();
+    }
+    public Rectangle getDescricaoLocation() {
+        return driver.findElement(descricao).getRect();
+    }
+    public  Rectangle getNotaLocation() {
+        return driver.findElement(nota).getRect();
+    }
+    public Rectangle getSubmitButtomLocation() {
+        return driver.findElement(submitButtom).getRect();
+    }
 }
