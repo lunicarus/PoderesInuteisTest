7a055d1cee2122521eac4c2e6d86d160880b61b3
lucas da silva cruz <lucas6jau@gmail.com>
2024-06-12 21:51:54 -0300

    fix: fixed ComponentReducer class, corrected logic for overlapping components

--- src/test/java/site/componentReducer.java
+++ src/test/java/site/componentReducer.java
@@ -3,39 +3,50 @@ package site;
 import org.openqa.selenium.Rectangle;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 class ComponentReducer {
-    private Set<String> seenLocations;
-    private Optional<Rectangle> overlappingComponent;
-
-    public ComponentReducer() {
-        this.seenLocations = new HashSet<>();
-        this.overlappingComponent = Optional.empty();
-    }
+    private Set<Rectangle> seenLocations = new HashSet<>();
+    private Rectangle overlappingComponent;
 
     public void addLocation(Rectangle component) {
-        seenLocations.add(component.toString());
+        seenLocations.add(component);
     }
 
     public boolean hasSeenLocation(Rectangle component) {
-        return seenLocations.contains(component.toString());
+        return seenLocations.contains(component);
     }
 
     public void setOverlappingComponent(Rectangle component) {
-        this.overlappingComponent = Optional.of(component);
+        for(Rectangle knownComponent : seenLocations) {
+            if (knownComponent.equals(component)) {
+                this.overlappingComponent = component;
+                break;
+            }
+            if(isOverlaping(component, knownComponent)) {
+                this.overlappingComponent = component;
+            }
+
+        }
+
+    }
+
+    private static boolean isOverlaping(Rectangle component, Rectangle knownComponent) {
+        return knownComponent.x < component.x + component.width &&
+                knownComponent.x + knownComponent.height > component.x &&
+                knownComponent.y < component.y + component.height &&
+                knownComponent.y + knownComponent.height > component.y;
     }
 
     public Optional<Rectangle> getOverlappingComponent() {
-        return overlappingComponent;
+        return Optional.ofNullable(overlappingComponent);
     }
 
     public ComponentReducer combine(ComponentReducer other) {
         this.seenLocations.addAll(other.seenLocations);
-        if (this.overlappingComponent.isEmpty()) {
-            this.overlappingComponent = other.overlappingComponent;
-        }
+        if (this.overlappingComponent == null) this.overlappingComponent = other.overlappingComponent;
         return this;
     }
 }
